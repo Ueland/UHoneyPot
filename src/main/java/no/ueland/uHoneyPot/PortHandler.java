6586ee42d4daa51e1d38fc0ed5d89253f4f9717c
Tor Henning Ueland <tor.henning@gmail.com>
2017-04-13 17:16:21 +0200

    PortHandler.java: Fixed bugs around socket creation

--- src/main/java/no/ueland/uHoneyPot/PortHandler.java
+++ src/main/java/no/ueland/uHoneyPot/PortHandler.java
@@ -1,48 +1,31 @@
 package no.ueland.uHoneyPot;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.util.Set;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Created by Ueland on 4/13/17.
  */
-public class PortHandler implements Runnable {
+class PortHandler implements Runnable {
 
-    private static Selector selector = null;
     private final int port;
 
     public PortHandler(int port) {
         this.port = port;
-        try {
-            selector = Selector.open();
-        } catch (IOException e) {
-            Util.die(e);
-        }
     }
 
     @Override
     public void run() {
         try {
-            ServerSocketChannel server = ServerSocketChannel.open();
-            server.configureBlocking(false);
-            server.socket().bind(new InetSocketAddress(port));
-            server.register(selector, SelectionKey.OP_ACCEPT);
-
-            while (selector.isOpen()) {
-                selector.select();
-                Set readyKeys = selector.selectedKeys();
-                for (Object readyKey : readyKeys) {
-                    SelectionKey key = (SelectionKey) readyKey;
-                    if (key.isAcceptable()) {
-                        new Thread(new SocketHandler(server.accept().socket())).start();
-                    }
+            ServerSocket listener = new ServerSocket(this.port);
+            try {
+                while (true) {
+                    Socket socket = listener.accept();
+                    new Thread(new SocketHandler(socket)).start();
                 }
+            } finally {
+                listener.close();
             }
-
         }catch(Exception ex) {
             Util.die(ex);
         }
